6cb65b88af56a10b7d3a56daa07516d9ec6331ad
Thupakula Meenakshi <meenakshithupakula@gmail.com>
2026-03-21 20:27:26 +0530

    Add files via upload

--- RelationalOP.java
+++ RelationalOP.java
@@ -2,12 +2,12 @@ class RelationalOP {
     public static void main(String s[]) {
         int a = 5;
         int b = 4;
-        //boolean result = a > b;
-        //boolean result = a < b;
-        //boolean result = a >= b;
-        //boolean result = a <= b;
-        //boolean result =  a==b;
-        boolean result = a!=b;
+        // boolean result = a > b;
+        // boolean result = a < b;
+        // boolean result = a >= b;
+        // boolean result = a <= b;
+        // boolean result = a==b;
+        boolean result = a != b;
         System.out.println(result);
     }
 }
